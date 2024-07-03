dea82d49e0985976ffb66d2e7af7d99c43d1032d
Harshit <harshit.23bcs10046@ms.sst.scaler.com>
2024-07-03 11:14:45 +0530

    implement logger

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
+// Logger middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 let courses = [
     { id: 1, name: 'Java' },
     { id: 2, name: 'JavaScript' },
